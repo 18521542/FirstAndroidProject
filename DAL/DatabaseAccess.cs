cee56e8d6c5e6bc6cce5dbe4d57e80d7bb818d57
TrongUIT <tranduytrong2000@gmail.com>
2020-11-01 12:46:01 +0700

    Add MySql connector, create class DatabaseAccess with single-ton design, connect success with mysql database

---
+++ DAL/DatabaseAccess.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace DAL
+{
+    class DatabaseAccess
+    {
+        //field
+        private string server = "localhost";
+        private string databaseName = "BookStoreManagement";
+        private string userName = "root";
+        private string passWord = "";
+        private string port = "3306";
+        public MySqlConnection conn = null;
+
+        private static DatabaseAccess instance = null;
+
+        
+        public static DatabaseAccess getInstance()
+        {
+            if (instance == null)
+                instance = new DatabaseAccess();
+            return instance;
+        }
+
+        public void getConnect()
+        {
+            string _server = "Server = " + server + ";";
+            string _dbName = "Database = " + databaseName + ";";
+            string UserID = "User ID = " + userName + ";";
+            string Pass = "Password = " + passWord + ";";
+            string _port = "Port=" + port;
+
+            //My sql conn string
+            string connStr = _server + _dbName + UserID + Pass + _port;
+
+            //start connecting
+            conn = new MySqlConnection(connStr);
+            conn.Open();
+        }
+        
+        public void getClose()
+        {
+            conn.Close();
+        }
+
+    }
+}
--- DAL/Program.cs
+++ DAL/Program.cs
@@ -3,13 +3,44 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using MySql.Data.MySqlClient;
 namespace DAL
 {
     class Program
     {
         static void Main(string[] args)
         {
+            try
+            {
+                DatabaseAccess.getInstance().getConnect();
+                Console.WriteLine("Ket noi thanh cong");
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine("Ket noi that bai");
+            }
+            finally
+            {
+                DatabaseAccess.getInstance().getClose();
+            }
+            
+            //MySqlConnection mySql = null;
+            //try
+            //{
+            //    mySql = new MySqlConnection(strConn);
+            //    mySql.Open();
+            //    Console.WriteLine("Ket noi thanh cong");
+            //}
+            //catch
+            //{
+            //    Console.WriteLine("Ket noi that bai");
+            //}
+            //finally
+            //{
+            //    mySql.Close();
+            //    mySql.Dispose();
+            //}
+            Console.ReadLine();
         }
     }
 }
